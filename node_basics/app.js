9f0a30d9d1dca205b66c374b7ac94667c1b5b96b
SSaratKS <sarat@s3kdevelopers.com>
2023-01-02 14:37:43 +0000

    Example of sharing data with minor changes to css

--- node_basics/app.js
+++ node_basics/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routers are filtered using '/admin'
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
