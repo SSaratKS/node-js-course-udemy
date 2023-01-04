6b2ccbdc9a44b7e8e9d044537673049081723432
SSaratKS <sarat@s3kdevelopers.com>
2023-01-04 12:05:31 +0000

    converted pug project to hbs

--- assignment-template-engine/app.js
+++ assignment-template-engine/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
-
-const bodyParser = require('body-parser');
-
 const users = [];
 
-app.set('view engine', 'pug');
+app.engine(
+  'hbs',
+  expressHbs.engine({ defaultLayout: 'main-layout', extname: 'hbs' })
+);
+app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,11 +20,16 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/users', (req, res, next) => {
-  res.render('users', { pageTitle: 'Users', users: users });
+  res.render('users', {
+    pageTitle: 'Users',
+    users: users,
+    hasUsers: users.length > 0,
+  });
 });
 
 app.post('/add-user', (req, res, next) => {
   users.push({ name: req.body.username });
+  console.log('Users', users);
   res.redirect('/users');
 });
 
